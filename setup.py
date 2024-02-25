4f430378ed993ce9d5eec1ad8a7dbec71f3bb6a0
‮ <gxousos@gmail.com>
2024-02-25 16:25:55 +0200

    Add `console_scripts`

--- setup.py
+++ setup.py
@@ -42,4 +42,9 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    entry_points={
+        'console_scripts': [
+            'pyotp = pyotp.__main__:main'
+        ]
+    },
 )
